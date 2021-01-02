cf9a5160db83d8f27f570d66738e460aff09524c
widada <wida.skydev@gmail.com>
2021-01-02 10:57:44 +0700

    first init

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [
+    {
+      script: 'server1.js',
+      name: 'server1',
+      watch: true
+    },
+    {
+      script: 'server2.js',
+      name: 'server2',
+      watch: true
+    },
+    {
+      script: 'server3.js',
+      name: 'server3',
+      watch: true
+    }
+  ]
+};
